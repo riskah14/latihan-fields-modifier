4a880ed3dcd0e46c7b8729a0b2c197889e68a3e3
riskah14 <riskah1412@gmail.com>
2018-10-05 09:56:29 +0700

    first commit

---
+++ Mobil.java
@@ -0,0 +1,36 @@
+public class Mobil{
+
+	int ban;	
+	int kecepatan;
+	int bensin;
+	String merk;
+
+	void maju(){
+		System.out.println("Mobil majuuu...");
+	}
+
+	void mundur(){
+		System.out.println("Mobil munduur...");
+	}
+
+	String showMerk(){
+		return Merk;
+	}
+	Void setMerk(String nama){
+		merk = nama;
+	}
+	// void addBensin(){
+	void addBensin(int n){
+		bensin += n;
+	}
+
+	int showBensin(){
+		return Bensin;
+	}
+
+	// }
+
+	// int showBensin(){
+	
+	// }
+}
---
+++ MobilTest.java
@@ -0,0 +1,13 @@
+public class MobilTest{
+	public static void main(String[] args) {
+		Mobil m1 = new Mobil();
+
+		m.maju(); //mobil majuuu..
+		m.mundur(); //mobil munduur..
+		m.ban = 4; //nothing show
+		System.out.println("ban = "+m.ban); //ban = 4
+		m.addBensin = 9000;
+		m.addMerk = "Toyota";		
+		System.out.println("merk ="+m.bensin);
+	}
+}
